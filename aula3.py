bb2739ba204bd3f19720923429b47589773870ab
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2020-06-26 16:40:16 -0300

    Create Blob Detector

--- aula3.py
+++ aula3.py
@@ -6,12 +6,38 @@ import matplotlib.pyplot as plt
 import cv2
 
 #Importando a imagem
-imagem = cv2.imread('imagem_virus2.png')
+imagem = cv2.imread('imagem_virus.png')
 imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
 
-#blob detection only works with white background
-detector = cv2.SimpleBlobDetector_create()
-saida = detector.detect(imagem)
+plt.imshow(imagem)
+plt.show()
+
+(thresh, imagem_tratada) = cv2.threshold(imagem, 127, 255, 0, cv2.THRESH_BINARY)
+plt.imshow(imagem_tratada)
+plt.show()
+
+def create_blob_detector(roi_size=(140, 140), blob_min_area=1, 
+                         blob_min_int=0, blob_max_int=.99, blob_th_step=1):
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = blob_min_area
+    params.maxArea = roi_size[0]*roi_size[1]
+    params.filterByCircularity = False
+    params.filterByColor = False
+    params.filterByConvexity = False
+    params.filterByInertia = False
+    # blob detection only works with "uint8" images.
+    params.minThreshold = int(blob_min_int*255)
+    params.maxThreshold = int(blob_max_int*255)
+    params.thresholdStep = blob_th_step
+    ver = (cv2.__version__).split('.')
+    if int(ver[0]) < 3:
+        return cv2.SimpleBlobDetector(params)
+    else:
+        return cv2.SimpleBlobDetector_create(params)
+
+detector = create_blob_detector()
+saida = detector.detect(imagem_tratada)
 
 number_of_blobs = len(saida)
 text = "Number of Blobs: " + str(len(saida))
