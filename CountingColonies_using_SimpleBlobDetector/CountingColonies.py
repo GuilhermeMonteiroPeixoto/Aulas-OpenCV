3ace2afd839372acb3536bcc65ec783b974b626f
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2020-11-03 13:12:07 -0300

    Update CountingColonies.py

--- CountingColonies_using_SimpleBlobDetector/CountingColonies.py
+++ CountingColonies_using_SimpleBlobDetector/CountingColonies.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import cv2
 
 #Importando a imagem
-imagem = cv2.imread('Bolor.png')
+imagem = cv2.imread('Bolor3.png')
 imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
 
 plt.imshow(imagem)
